ded0e56c71d9d900fc8fb14b0c5870714a9044c9
azmisahin <6445951+azmisahin@users.noreply.github.com>
2026-04-18 16:51:08 +0000

    Deploying to gh-pages from @ sentiric/sentiric-stream-sdk@31ebee4203cbfdf960d432586fe61532f1a7fece 🚀

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-import"./assets/modulepreload-polyfill-CNgwewHt.js";document.getElementById("app-version").innerText="SDK v0.8.2";
+import"./assets/modulepreload-polyfill-CNgwewHt.js";document.getElementById("app-version").innerText="SDK v0.8.3";
